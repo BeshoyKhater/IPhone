c92460fb33e262923f9137d59db8eec29e3f35ad
BeshoyKhater <BeshoyKhater@gmail.com>
2023-02-08 17:45:49 +0200

    First Commit

---
+++ test.js
@@ -0,0 +1,8 @@
+let img=document.querySelector('.pic')
+let container=document.querySelector('.container')
+function phones(x){
+    img.src=x;
+}
+function color(y){
+    container.style.background=y
+}
